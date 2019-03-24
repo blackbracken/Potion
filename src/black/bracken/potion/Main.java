dcf7d23ef5d4cecad531a98247391a6398017d23
BlackBracken <me@bracken.black>
2019-03-25 08:58:01 +0900

    initialize project

---
+++ src/black/bracken/potion/Main.java
@@ -0,0 +1,14 @@
+package black.bracken.potion;
+
+/**
+ * @author BlackBracken
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
